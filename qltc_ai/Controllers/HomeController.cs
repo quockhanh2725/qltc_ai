5b35ec0289118474931c551ee20e2b4de6700e4e
quockhanh <animelylia@gmail.com>
2026-04-01 11:26:13 +0700

    test connect database

---
+++ qltc_ai/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using qltc_ai.Models;
+using System;
+
+namespace qltc_ai.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly qltcContext _context;
+
+        public HomeController(qltcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            bool isConnected = false;
+
+            try
+            {
+                isConnected = _context.Database.CanConnect();
+            }
+            catch
+            {
+                isConnected = false;
+            }
+
+            return View(isConnected);
+        }
+    }
+}
