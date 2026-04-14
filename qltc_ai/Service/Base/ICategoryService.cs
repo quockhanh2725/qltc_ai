0e58fe3859958e9947d629aaed6b9095016f4588
quockhanh <animelylia@gmail.com>
2026-04-14 09:10:26 +0700

    update newlimit category

---
+++ qltc_ai/Service/Base/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using qltc_ai.Models;
+
+namespace qltc_ai.Service.Base
+{
+    public interface ICategoryService
+    {
+        List<Danhmuc> GetAllCatrgory(int id);
+        (bool success, decimal thieu) UpdateLimit(int accId, int idCate, decimal newLimit);
+    }
+}
