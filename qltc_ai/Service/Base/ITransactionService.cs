865cddc1acc6edac7f4f744a332f1bdf7cf9545d
quockhanh <animelylia@gmail.com>
2026-04-15 16:30:23 +0700

    update transaction

--- qltc_ai/Service/Base/ITransactionService.cs
+++ qltc_ai/Service/Base/ITransactionService.cs
@@ -3,5 +3,7 @@
     public interface ITransactionService
     {
         bool AddTransaction(int accid, int idCate, decimal money, string note);
+        bool UpdateTransaction(int accid , int idTran , decimal newMoney, string newNote);
+        //bool DeleteTransaction(int accid, int idTran);
     }
 }
