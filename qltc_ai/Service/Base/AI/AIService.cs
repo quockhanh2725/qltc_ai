34c3e06a1413f4115b8b5b1a0253e9d3977341a7
quockhanh <animelylia@gmail.com>
2026-05-14 16:10:36 +0700

    train models

--- qltc_ai/Service/Base/AI/AIService.cs
+++ qltc_ai/Service/Base/AI/AIService.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using Microsoft.ML.Data;
+using Microsoft.ML;
+using qltc_ai.Models.AI;
+using qltc_ai.Service.Base.AI;
+
+public class AIService
+{
+    private ITransformer _model;
+    private MLContext _ml;
+    private PredictionEngine<TransactionInput, TransactionPrediction> _engine;
+    private DataViewSchema _schema;
+
+    public void Init()
+    {
+        _ml = new MLContext();
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "data", "model.zip");
+
+        if (!File.Exists(path))
+            MLModelsTrainer.Train();
+
+
+        _model = _ml.Model.Load(path, out _schema);
+
+
+        _engine = _ml.Model.CreatePredictionEngine<TransactionInput, TransactionPrediction>(
+            _model, inputSchemaDefinition: SchemaDefinition.Create(typeof(TransactionInput)));
+    }
+
+    public int PredictCategory(string text)
+    {
+        var result = _engine.Predict(new TransactionInput { Text = text });
+        return int.Parse(result.PredictedLabel!);
+    }
+}
--- qltc_ai/Service/Base/AI/MLModelsTrainer.cs
+++ qltc_ai/Service/Base/AI/MLModelsTrainer.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using Microsoft.ML;
+using qltc_ai.Models.AI;
+
+namespace qltc_ai.Service.Base.AI
+{
+    public class MLModelsTrainer
+    {
+        public static void Train()
+        {
+            var ml = new MLContext();
+
+            var data = ml.Data.LoadFromTextFile<TransactionData>(
+                path: "data/data.txt",
+                hasHeader: false,
+                separatorChar: '|'
+            );
+
+            var pipeline =
+    ml.Transforms.Text.FeaturizeText(
+        outputColumnName: "Features",
+        inputColumnName: nameof(TransactionData.Text))
+    .Append(
+        ml.Transforms.Conversion.MapValueToKey(
+            outputColumnName: "Label",
+            inputColumnName: nameof(TransactionData.Label)))
+    .Append(
+        ml.MulticlassClassification.Trainers.SdcaMaximumEntropy(
+            labelColumnName: "Label",
+            featureColumnName: "Features"))
+    .Append(
+        ml.Transforms.Conversion.MapKeyToValue(
+            outputColumnName: "PredictedLabel",
+            inputColumnName: "PredictedLabel"));
+
+            var model = pipeline.Fit(data);
+            ml.Model.Save(model, data.Schema, "data/model.zip");
+        }
+    }
+}
