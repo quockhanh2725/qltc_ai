3dc5cb5fba7ce0a75dbf7db68c4c1ccb19d714c5
quockhanh <animelylia@gmail.com>
2026-06-04 13:57:41 +0700

    update chat

--- qltc_ai/Program.cs
+++ qltc_ai/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using qltc_ai.Middlewares;
 using qltc_ai.Models;
 using qltc_ai.Repositories;
 using qltc_ai.Service;
 using qltc_ai.Service.Base;
-//using qltc_ai.Service.Base.AI;
+using qltc_ai.Service.Base.AI;
 using System.ComponentModel;
 using System.Text.Json.Serialization;
 
@@ -23,10 +24,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSession();
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddDbContext<qltcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySqlConnection"))));
+
+/*builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.GeneralRules = new List<RateLimitRule>
+    {
+        new RateLimitRule
+        {
+            Endpoint = "POST:/login",
+            Period = "5m",
+            Limit = 10
+        }
+    };
+});*/
 
 //Repository
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
@@ -36,6 +52,7 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IChatRepository, ChatRepository>();
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
 
 //Service
 builder.Services.AddScoped<IAccountService, AccountService>();
@@ -44,17 +61,20 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IBudgetService, BudgetService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IInvoiceScanService, InvoiceScanService>();
+builder.Services.AddScoped<IChatService, ChatService>();
 
 
-//builder.Services.AddSingleton<AIService>();
+builder.Services.AddSingleton<AIService>();
 builder.Services.AddSingleton<EmailService>();
-//builder.Services.AddSingleton<ChatParserService>();
-//builder.Services.AddSingleton<MLModelsTrainer>();
+builder.Services.AddSingleton<ChatParserService>();
+builder.Services.AddSingleton<MLModelsTrainer>();
+builder.Services.AddSingleton<IScanTokenService , ScanTokenService>();
 
 var app = builder.Build();
 
-//var ai = app.Services.GetRequiredService<AIService>();
-//ai.Init();
+var ai = app.Services.GetRequiredService<AIService>();
+ai.Init();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -65,6 +85,8 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseSession();
+
+app.UseMiddleware<AuthorizationMiddleware>();
 
 app.UseAuthorization();
 
